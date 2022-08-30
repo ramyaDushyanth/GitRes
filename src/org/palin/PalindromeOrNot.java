4fc191771258df73d12e7e07c54e02fbaffc9b99
ramyaDushyanth <ramyasastika@gmail.com>
2022-08-30 15:07:38 +0530

    Lines of code added

---
+++ src/org/palin/PalindromeOrNot.java
@@ -0,0 +1,28 @@
+package org.palin;
+
+import java.util.Scanner;
+
+public class PalindromeOrNot {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("\nTo check palindrome or not\n==========================");
+	System.out.println("Enter the word:");
+	String word = s.next();
+	String rev = "";
+	for (int i = word.length() - 1; i >= 0; i--) {
+		char ch = word.charAt(i);
+		rev = rev + ch;
+	}
+	System.out.println("Given String:" + word);
+	System.out.println("Reversed String:" + rev);
+    if (word.equals(rev)) {
+		System.out.println("Palindrome");
+
+	} else {
+		System.out.println("Not palindrome");
+	}
+
+}
+
+}
+
